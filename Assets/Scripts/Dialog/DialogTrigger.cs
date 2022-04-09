b1a0ff469a5c6be4d270a08a87f162cf152dc085
Gustavo Lucas da Rosa <gustavolucasdarosa@gmail.com>
2022-04-09 18:13:49 -0300

    removed comments

--- Assets/Scripts/Dialog/DialogTrigger.cs
+++ Assets/Scripts/Dialog/DialogTrigger.cs
@@ -7,18 +7,6 @@ public class DialogTrigger : MonoBehaviour
     public Dialog dialog;
     public DialogManager dialogManager;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void TriggerDialog()
     {
         Debug.Log("calling dialog manager");
