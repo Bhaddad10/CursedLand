2dbd2d026bb31a2e57b5feb7ecc9a4428d749c39
Bruno Haddad <brunolemoshaddad@gmail.com>
2022-04-29 22:06:04 -0300

    Movimentacao e animacao do inimigo

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private Animator animator;
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
+    private Transform target;
 
     public int health = 100;
-
+    public float speed = 9;
+    private float range = 5;
     private void Awake()
     {
-        animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+    public void Update()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= range)
+        {
+            followPlayer();
+        }
+    }
+    public void followPlayer()
+    {
+        _animator.SetBool("isMoving", true);
+        _animator.SetFloat("moveX", target.position.x - transform.position.x);
+        _animator.SetFloat("moveY", target.position.y - transform.position.y);
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
     }
     public void takeDamage(int damage)
     {
@@ -24,9 +43,8 @@ public class EnemyController : MonoBehaviour
     public void die()
     {
         Debug.Log("inimigo morreu");
-        animator.SetBool("isDead", true);
+        _animator.SetBool("isDead", true);
         Destroy(gameObject, 5f);
     }
-
-    //Código inspirado e adaptado deste video : https://www.youtube.com/watch?v=wkKsl1Mfp5M&t
+    //Logica de receber dano e morrer inspirado e adaptado deste video : https://www.youtube.com/watch?v=wkKsl1Mfp5M&t
 }
