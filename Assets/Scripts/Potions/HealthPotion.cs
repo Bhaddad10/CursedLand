fc097fcb35d755b79718be6d0cd0214dce760c57
Gustavo Lucas da Rosa <gustavolucasdarosa@gmail.com>
2022-06-10 16:26:03 -0300

    Refactored Potions

--- Assets/Scripts/Potions/HealthPotion.cs
+++ Assets/Scripts/Potions/HealthPotion.cs
@@ -9,7 +9,6 @@ public class HealthPotion : Potion
     public HealthPotion(int quantity, Sprite sprite) : base(quantity, sprite) { }
     internal override bool Consume()
     {
-        //Debug.Log("Consuming Health Potion..");
         GameManager.Instance.playerState.restoreHp(healthToRestore);
         return true;
     }
